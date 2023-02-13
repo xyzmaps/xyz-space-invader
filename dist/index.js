24a45e1cf855771d5472a9691b1937bf53e3f677
Oliver Fink <323250+olifink@users.noreply.github.com>
2023-02-13 15:31:57 +0100

    publish build

--- dist/index.js
+++ dist/index.js
@@ -44849,7 +44849,7 @@
 
 	function applyTags({ spaceId, tagFilterQueryParam, hexbinInfo, displayToggles: { hexbins } = {} }, scene_config) {
 	  // choose selected main space tags, or hexbin-specific tag
-	  const hubApi = AppUI.get().hubApi;
+	  const hubApi = appUI.get().hubApi;
 	  let activeTags = tagFilterQueryParam;
 	  var currentZoom = scene.view.tile_zoom; // or map.getZoom() ?
 	  if (hexbins === 1 & Object.keys(hexbinInfo).length > 0) { // ensure hexbins info exists
@@ -44918,7 +44918,7 @@
 	}
 
 	async function getStats({ spaceId, token, mapStartLocation }) {
-	  const hubApi = AppUI.get().hubApi;
+	  const hubApi = appUI.get().hubApi;
 	  // Get stats endpoint
 	  var url = `{hubApi}/spaces/${spaceId}/statistics`;
 	  const stats = await fetch(url).then(r => r.json());
